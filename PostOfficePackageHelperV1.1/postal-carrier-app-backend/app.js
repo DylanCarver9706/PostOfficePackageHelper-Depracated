039f696661dff03f18d2de96266c17647d3c6a88
Dylan Carver <dylancarver14@gmail.com>
2023-10-20 12:55:14 -0500

    Case builder POSTs working

--- PostOfficePackageHelperV1.1/postal-carrier-app-backend/app.js
+++ PostOfficePackageHelperV1.1/postal-carrier-app-backend/app.js
@@ -1,30 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mysql = require('mysql2');
-const bcrypt = require('bcrypt');
+const mysql = require("mysql2");
+const bcrypt = require("bcrypt");
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
 // Create the MySQL database connection
 const db = mysql.createConnection({
-  host: 'localhost',         // Replace with your MySQL host
-  user: 'root',              // Replace with your MySQL username
-  password: 'admin',         // Replace with your MySQL password
-  database: 'PostOfficePackageHelperV1.1',  // Replace with your MySQL database name
+  host: "localhost", // Replace with your MySQL host
+  user: "root", // Replace with your MySQL username
+  password: "admin", // Replace with your MySQL password
+  database: "PostOfficePackageHelperV1.1", // Replace with your MySQL database name
 });
 
 // Connect to the MySQL database
 db.connect((err) => {
   if (err) {
-    console.error('Error connecting to MySQL:', err);
+    console.error("Error connecting to MySQL:", err);
     return;
   }
-  console.log('Connected to MySQL database');
+  console.log("Connected to MySQL database");
 });
 
-// Define a route to handle user registration
-app.post('/api/register', async (req, res) => {
+app.post("/api/register", async (req, res) => {
   try {
     const { first_name, last_name, email, password } = req.body;
 
@@ -32,19 +31,128 @@ app.post('/api/register', async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     // Insert the user's information into the database
-    const sql = 'INSERT INTO users (first_name, last_name, email, password) VALUES (?, ?, ?, ?)';
-    db.query(sql, [first_name, last_name, email, hashedPassword], (err, result) => {
-      if (err) {
-        console.error('Error registering user:', err);
-        return res.status(500).json({ error: 'An error occurred while registering the user.' });
+    const sql =
+      "INSERT INTO users (first_name, last_name, email, password) VALUES (?, ?, ?, ?)";
+    db.query(
+      sql,
+      [first_name, last_name, email, hashedPassword],
+      (err, result) => {
+        if (err) {
+          console.error("Error registering user:", err);
+          return res
+            .status(500)
+            .json({ error: "An error occurred while registering the user." });
+        }
+        console.log("User registered successfully");
+        res.status(201).json({ message: "User registered successfully" });
       }
-      console.log('User registered successfully');
-      res.status(201).json({ message: 'User registered successfully' });
-    });
+    );
   } catch (error) {
-    console.error('Error registering user:', error);
-    res.status(500).json({ error: 'An error occurred while registering the user.' });
+    console.error("Error registering user:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while registering the user." });
   }
+});
+
+// Define a route to create a new route
+app.post("/api/routes", (req, res) => {
+  // Extract route data from the request body
+  const { user_id, route_number } = req.body;
+
+  // Insert the route data into the database
+  const sql = "INSERT INTO routes (user_id, route_number) VALUES (?, ?)";
+  db.query(sql, [user_id, route_number], (err, result) => {
+    if (err) {
+      console.error("Error creating route:", err);
+      return res
+        .status(500)
+        .json({ error: "An error occurred while creating the route." });
+    }
+    console.log("Route created successfully");
+    res.status(201).json({ message: "Route created successfully" });
+  });
+});
+
+// Define a route to create a new case
+app.post("/api/cases", (req, res) => {
+  // Extract case data from the request body
+  const { route_id, case_number } = req.body;
+
+  // Insert the case data into the database
+  const sql = "INSERT INTO cases (route_id, case_number) VALUES (?, ?)";
+  db.query(sql, [route_id, case_number], (err, result) => {
+    if (err) {
+      console.error("Error creating case:", err);
+      return res
+        .status(500)
+        .json({ error: "An error occurred while creating the case." });
+    }
+    console.log("Case created successfully");
+    res.status(201).json({ message: "Case created successfully" });
+  });
+});
+
+// Define a route to create a new row
+app.post("/api/case_rows", (req, res) => {
+  // Extract row data from the request body
+  const { case_id, case_row_number } = req.body;
+
+  // Insert the row data into the database
+  const sql =
+    "INSERT INTO `case_rows` (case_id, case_row_number) VALUES (?, ?)";
+
+  db.query(sql, [case_id, case_row_number], (err, result) => {
+    if (err) {
+      console.error("Error creating case row:", err);
+      return res
+        .status(500)
+        .json({ error: "An error occurred while creating the case row." });
+    }
+    console.log("Case row created successfully");
+    res.status(201).json({ message: "Case row created successfully" });
+  });
+});
+
+// Define a route to create a new address
+app.post("/api/addresses", (req, res) => {
+  // Extract address data from the request body
+  const {
+    case_row_id,
+    number,
+    address1,
+    address2,
+    city,
+    state,
+    zip_code,
+    /* additional address data */
+  } = req.body;
+
+  // Insert the address data into the database
+  const sql =
+    "INSERT INTO addresses (case_row_id, number, address1, address2, city, state, zip_code) VALUES (?, ?, ?, ?, ?, ?, ?)";
+  db.query(
+    sql,
+    [
+      case_row_id,
+      number,
+      address1,
+      address2,
+      city,
+      state,
+      zip_code /* additional values */,
+    ],
+    (err, result) => {
+      if (err) {
+        console.error("Error creating address:", err);
+        return res
+          .status(500)
+          .json({ error: "An error occurred while creating the address." });
+      }
+      console.log("Address created successfully");
+      res.status(201).json({ message: "Address created successfully" });
+    }
+  );
 });
 
 // Define other routes and middleware as needed
